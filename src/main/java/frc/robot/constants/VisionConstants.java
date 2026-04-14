2dfb3b13346019dfe995873aa178618db59ddbd2
ncgears <1918ncgears@gmail.com>
2026-04-14 19:06:25 -0400

    Update cameras

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -73,12 +73,12 @@ public class VisionConstants {
 
     public static final class Front { //Right facing camera 2026 *****
         public static final String kCameraName = "frontcam";
-        public static final boolean kUseForPose = false;
+        public static final boolean kUseForPose = true;
         //+x left from center, +y forward from center, +z up from ground
         public static final Transform3d kRobotToCam = new Transform3d(
           new Translation3d(-0.0864,0.295,0.51), //x,y,z location of camera on robot in meters
           //Rotation3d.kZero //yaw,pitch/roll of camera on robot in radians
-          new Rotation3d(0,0,Math.toRadians(0)) //yaw,pitch/roll of camera on robot in radians
+          new Rotation3d(0,Math.toRadians(30),Math.toRadians(0)) //yaw,pitch/roll of camera on robot in radians
         );
     }
     public static final class Back { //Back facing camera 2026 *****
